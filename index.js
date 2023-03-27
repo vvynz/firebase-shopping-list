1ba255e5282031406524e6cd2883bfa211b25f2d
Vivian Zhang <vvynz12@gmail.com>
2023-03-27 14:57:54 -0400

    import functions from firebase and setup an app and database variables

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-import { add } from "./functions.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
   dbURL: "https://shopping-cart-c5c06-default-rtdb.firebaseio.com/",
 }
 
-console.log(add(2,4))
+const app = initializeApp(appSettings);
+const database = getDatabase(app)
+
+console.log(app)
 
 const inputEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
