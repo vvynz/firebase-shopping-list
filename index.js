e6e558d7781a7acbad11ed038c204c36e5dbd585
Vivian Zhang <vvynz12@gmail.com>
2023-03-27 14:47:28 -0400

    setup appSettings with Firebase database url & create a functions.js file

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import { add } from "./functions.js"
+
+const appSettings = {
+  dbURL: "https://shopping-cart-c5c06-default-rtdb.firebaseio.com/",
+}
+
+console.log(add(2,4))
+
 const inputEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
 
