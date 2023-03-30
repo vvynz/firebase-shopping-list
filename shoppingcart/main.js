a569f9e2d03e6a8f7f6fb91195ac9fb2d7b0e6d1
Vivian Zhang <vvynz12@gmail.com>
2023-03-29 20:56:01 -0400

    setup firebase database & database configurations

--- shoppingcart/main.js
+++ shoppingcart/main.js
@@ -1,7 +1,20 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { initializeApp } from "firebase/app";
+// "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref } from "firebase/database";
+// "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
+  projectId: 'shoppinglist-50282',
   dbURL: "https://shoppinglist-50282-default-rtdb.europe-west1.firebasedatabase.app/",
 }
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+
+console.log(app);
+
+const inputEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+
+addButtonEl.addEventListener("click", addToCart);
 
