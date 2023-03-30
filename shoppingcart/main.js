d8161af90ebbfa3b074ab4cffb11edab3e631f58
Vivian Zhang <vvynz12@gmail.com>
2023-03-29 20:52:17 -0400

    install firebase dependency

--- shoppingcart/main.js
+++ shoppingcart/main.js
@@ -1,1 +1,7 @@
-import './style.css'
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  dbURL: "https://shoppinglist-50282-default-rtdb.europe-west1.firebasedatabase.app/",
+}
+
