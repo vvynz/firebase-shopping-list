71352ede677bb6a368086375ab0689dc178bce56
Vivian Zhang <vvynz12@gmail.com>
2023-03-29 21:10:10 -0400

    update database url

--- shoppingcart/main.js
+++ shoppingcart/main.js
@@ -1,18 +1,16 @@
-import { initializeApp } from "firebase/app";
-// "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref } from "firebase/database";
-// "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-  projectId: 'shoppinglist-50282',
-  dbURL: "https://shoppinglist-50282-default-rtdb.europe-west1.firebasedatabase.app/",
+  projectId: 'shoppinglist-68583',
+  dbURL: "https://shoppinglist-68583-default-rtdb.firebaseio.com/",
 }
 
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const shoppingListInDB = ref(database, "shoppingList");
 
-console.log(app);
+//console.log(app);
 
 const inputEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
@@ -22,6 +20,6 @@ addButtonEl.addEventListener("click", addToCart);
 function addToCart() {
   let inputValue = inputEl.value;
 
-
+  push(shoppingListInDB, inputValue);
   console.log(`${inputValue} was added!`);
 }
