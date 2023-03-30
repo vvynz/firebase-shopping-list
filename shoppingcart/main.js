442bd99a255c26d1a8b6ea8a869a5a2ec3caa604
Vivian Zhang <vvynz12@gmail.com>
2023-03-29 21:59:49 -0400

    update the addToCart function so that it appends the new values to the list

--- shoppingcart/main.js
+++ shoppingcart/main.js
@@ -1,10 +1,14 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-  projectId: 'shoppinglist-68583',
+  projectId: "shoppinglist-68583",
   dbURL: "https://shoppinglist-68583-default-rtdb.firebaseio.com/",
-}
+};
 
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
@@ -14,6 +18,7 @@ const shoppingListInDB = ref(database, "shoppingList");
 
 const inputEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
+const listEl = document.getElementById("shopping-list");
 
 addButtonEl.addEventListener("click", addToCart);
 
@@ -22,4 +27,13 @@ function addToCart() {
 
   push(shoppingListInDB, inputValue);
   console.log(`${inputValue} was added!`);
+
+  // my initial appreach
+  //   let listItemEl = document.createElement("li");
+
+  //   listItemEl.innerHTML = inputValue;
+  //   listEl.appendChild(listItemEl);
+
+  inputEl.value = "";
+  listEl.innerHTML += `<li>${inputValue}</li>`;
 }
